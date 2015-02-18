57c8d04848fe8d421a308f457d5d7f863e09ded4
kordano <konrad.kuehne@rocketmail.com>
2015-02-18 12:19:14 +0100

    fix that cheshire problem

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject ceres-analytics "0.1.0-SNAPSHOT"
 
-
   :description "Basic analytics of ceres collections"
 
   :url "http://example.com/FIXME"
@@ -9,12 +8,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2227"]
-                 [org.clojure/data.json "0.2.4"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [clj-time "0.7.0"]
                  [enlive "1.1.5"]
                  [gg4clj "0.1.0"]
+                 [cheshire "5.4.0"]
                  [com.datomic/datomic-free "0.9.4899"]
                  [com.novemberain/monger "2.0.0-rc1"]
                  [incanter "1.5.5"]
