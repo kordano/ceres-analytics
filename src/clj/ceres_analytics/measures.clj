5a201f7620b98019c0f85d8ee2c7a97173707b06
kordano <konrad.kuehne@rocketmail.com>
2015-04-22 13:30:03 +0200

    add neighborhood, degree and lifetime

--- src/clj/ceres_analytics/measures.clj
+++ src/clj/ceres_analytics/measures.clj
@@ -3,19 +3,23 @@
   (:require [ceres-analytics.db :refer [db]]
             [monger.collection :as mc]
             [monger.joda-time]
+            [clj-time.core :as t]
             [monger.operators :refer :all]
             [monger.query :refer :all]))
 
+(def contacts ["shares" "replies" "retweets" "tagrefs" "pubs"])
+(def cascades ["shares" "replies" "retweets"])
+(def nodes ["users" "messages" "tags"])
 
 (defn dispatch-entity [entity]
   (case entity
-    :nodes ["users" "messages" "tags"]
+    :nodes nodes
     :users "users"
     :messages "messages"
     :topics "tags"
-    :contacts ["shares" "replies" "retweets" "tagrefs" "pubs"]
+    :contacts contacts
     :publications "pubs"
-    :cascades ["shares" "replies" "retweets"]
+    :cascades cascades
     :assignments "tagrefs"
     :unrelated))
 
@@ -26,5 +30,33 @@
   [entity t1 t2]
   (let [colls (dispatch-entity entity)]
     (if (vector? colls)
-      (reduce + (map #(mc/count @db %) colls))
-      (mc/count @db colls))))
+      (zipmap colls
+              (map #(mc/find-maps @db % {:ts {$gt t1
+                                              $lt t2}}) colls))
+      {colls
+       (mc/find-maps @db colls {:ts {$gt t1
+                                     $lt t2}})})))
+
+
+(defn neighborhood
+  "Compute neighborhood of given node id"
+  [id t0]
+  (zipmap
+   contacts
+   (map #(mc/find-maps @db % {$or [{:ts {$lt t0}
+                                    :source id}
+                                   {:ts {$lt t0}
+                                    :target id}]}) contacts)))
+
+
+(defn degree
+  "Compute degree of given node id"
+  [id t0]
+  (->> (neighborhood id t0) vals (apply concat) count))
+
+
+(defn lifetime
+  "Computes lifetime of contact set"
+  [cs]
+  (let [sorted-cs (sort-by :ts cs)]
+    (t/in-seconds (t/interval (last cs) (first cs)))))
