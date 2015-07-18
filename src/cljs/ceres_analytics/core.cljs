2840770c780991a6308ecf23728a45c1290a38c0
kordano <konrad.kuehne@rocketmail.com>
2015-07-18 14:26:40 +0200

    remove print button and functionality

--- src/cljs/ceres_analytics/core.cljs
+++ src/cljs/ceres_analytics/core.cljs
@@ -4,19 +4,34 @@
             [cljs.reader :as reader]
             [cljs-time.core :as t]
             [cljs-time.coerce :as c]
-            [cljs.core.async :refer [<! >! put! close! timeout]])
+            [cljs.core.async :refer [<! >! chan put! close! timeout]])
   (:require-macros [cljs.core.async.macros :refer [go-loop go]]))
 
 (enable-console-print!)
 
 (defn log [m & ms] (.log js/console (apply str m ms)) )
 
+(def broadcasters
+  {2834511 "Spiegel"
+   5494392 "Focus"
+   5734902 "Tagesschau"
+   8720562 "Welt"
+   9204502 "Bild"
+   15071293 "Westen"
+   15243812 "TAZ"
+   ;15738602 "N24"
+   18016521 "FAZ"
+   18774524 "Stern"
+   19232587 "ntv"
+   40227292 "dpa"
+   114508061 "SZ"
+   1101354170 "ZDF"})
 
 (strokes/bootstrap)
 
 (def graph-state (atom {:svg nil
-                        :width 2000
-                        :height 2000
+                        :width 650
+                        :height 650
                         :frame "#graph-container"
                         :color nil
                         :force nil
@@ -53,7 +68,7 @@
                          (call (.-drag force)))]
       (.. node-enter
           (append "svg:circle")
-          (attr {:class "node-stroke" :r 5})
+          (attr {:class "node-stroke" :r 4})
           (style {:fill (fn [d] (color (dec (:group d))))
                   :stroke "#fff"}))
       (.. node-enter (append "title") (text (fn [d] (:value d)))))
@@ -74,7 +89,7 @@
     (.. force
         (charge -550)
         (gravity 0.5)
-        (linkDistance 15)
+        (linkDistance 10)
         (friction 0.1)
         (size [width height])
         (nodes (:nodes data))
@@ -107,6 +122,8 @@
                                       (select frame)
                                       (append "svg")
                                       (attr {:width width
+                                             :xmlns "http://www.w3.org/2000/svg"
+                                             :id "graph-svg"
                                              :height height})))
     (swap! state assoc-in [:force] force)
     (swap! state assoc-in [:color] ["purple" "steelblue" "orange" "green" "red" ])
@@ -116,10 +133,9 @@
 (defn start-vis [state]
   (let [nodes (get-in @state [:data :new-nodes])
         links (get-in @state [:data :new-links])]
-    (println "Starting vis...")
     (go-loop [k 10]
       (if (= k 11)
-        (println "done")
+        (set! (.-innerHTML (.getElementById js/document "current-time")) "DONE")
         (do
           (loop [i 0]
             (if (= i 24)
@@ -129,7 +145,7 @@
                   (if (= j 60)
                     nil
                     (do
-                      (println (str i ":" j))
+                      (set! (.-innerHTML (.getElementById js/document "current-time")) (str "Time: " i ":" (if (= j 0) "00" j)))
                       (<! (timeout 2000))
                       (let [k (t/interval (t/date-time 2015 4 k i j) (t/date-time 2015 4 k i (+ j 20)))
                             new-nodes (filter #(t/within? k (:ts %)) nodes)
@@ -144,9 +160,11 @@
 (defn run [state]
   (init-graph state)
   (go
-    (let [{:keys [ws-channel error]} (<! (ws-ch "ws://localhost:8091/data/ws"))]
+    (let [{:keys [ws-channel error]} (<! (ws-ch "ws://localhost:8091/data/ws"))
+          broadcaster (rand-nth (keys broadcasters))]
       (swap! state assoc-in [:ws-channel] ws-channel)
-      (>! ws-channel {:topic :user-tree :data 5734902})
+      (>! ws-channel {:topic :user-tree :data broadcaster})
+      (set! (.-innerHTML (.getElementById js/document "graph-title")) (get broadcasters broadcaster))
       (if-not error
         (loop [{:keys [message error] :as in} (<! ws-channel)]
           (when in
@@ -156,9 +174,6 @@
                 (let [{:keys [nodes links]} message
                       formated-nodes (map (fn [n] (update-in n [:ts] c/from-long)) nodes)
                       formated-links (map (fn [l] (update-in l [:ts] c/from-long)) links)]
-                  (println message)
-                  (println formatted-nodes)
-                  (println formatted-links)
                   (swap! state assoc-in [:data :new-nodes] formated-nodes)
                   (swap! state assoc-in [:data :new-links] formated-links)
                   (<! (timeout 1000))
