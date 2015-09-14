092c08b79d0f9827d243a5832ea38ce57465ab50
kordano <konrad.kuehne@rocketmail.com>
2015-09-14 17:44:51 +0200

    bump it

--- import-all.sh
+++ import-all.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
+for i in `seq 1 9`;
+do
+    d=2015-03-0$i
+    sh import-date.sh $d
+    echo "IMPORTING $d"
+done  
+
 for i in `seq 10 31`;
 do
-    sh import-date.sh 2015-03-$i
+    d=2015-03-$i
+    sh import-date.sh $d
+    echo "IMPORTING $d"
 done  
+
+for i in `seq 1 9`;
+do
+    d=2015-04-0$i
+    sh import-date.sh $d
+    echo "IMPORTING $d"
+done  
+
+for i in `seq 10 30`;
+do
+    d=2015-04-0$i
+    sh import-date.sh $d
+    echo "IMPORTING $d"
+done  
+
+for i in `seq 1 9`;
+do
+    d=2015-05-0$i
+    sh import-date.sh $d
+    echo "IMPORTING $d"
+done  
+
